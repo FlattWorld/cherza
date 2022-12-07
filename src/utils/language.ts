e8a207a9ddd762355b2e5ca40d551fff9a167bd3
Lino09 <onosmx@gmail.com>
2022-12-06 22:36:26 -0600

    feat: add languages file

---
+++ src/utils/language.ts
@@ -0,0 +1,117 @@
+export const header = {
+  EN: ['Home', 'Services', 'About us', 'Book a trip'],
+  ES: ['Inicio', 'Servicios', 'Quienes somos', 'Reservar'],
+}
+export const hero = {
+  EN: {
+    title: ['Travel fast ', 'and ', 'safely'],
+    des: ['Book a service in minutes,', "we'll take care of the rest."],
+  },
+  ES: {
+    title: ['Viaja rápido y ', 'con ', 'seguridad'],
+    des: ['Reserva un viaje en minutos,', 'nosotros nos encargamos del resto.'],
+  },
+}
+export const services = {
+  EN: {
+    h2: ['Where are we going?', 'Where ', 'are we ', 'going?'],
+    cards: [
+      [
+        'Book a Driver',
+        'In your car, on your own way, without any kind of worries.',
+      ],
+      [
+        'Book a Trip',
+        'Travel safely and comfortably, we will provide a great experience.',
+      ],
+      ['Book Package transport', 'We can get anywhere, fast and secure.'],
+    ],
+    button: 'Book it!',
+  },
+  ES: {
+    h2: ['¿A dónde vamos?', '¿A ', 'dónde ', 'vamos?'],
+    cards: [
+      [
+        'Reservar Chofer',
+        'En tu auto, a tu manera, sin preocupaciones de ningún tipo.',
+      ],
+      [
+        'Reservar Viaje',
+        'Viaja con seguridad y comodidad, tu experiencia con nosotros será inolvidable.',
+      ],
+      ['Reservar Paquetería', 'Llegamos a todas partes, rápido y seguro.'],
+    ],
+    button: '¡Quiero reservar!',
+  },
+}
+export const somosCherza = {
+  EN: {
+    h3: ['We are ', 'Cherza'],
+    p: 'The transport agency that you were looking for. We can take you to your dream trip, a party with friends or a medical appointment tha you cannot localStorage. You can trust in cherza!',
+    services: [
+      { title: 'Airport', desc: 'We take you to your next adventure!' },
+      { title: 'Hospitals', desc: "Don't miss your medical appointment." },
+      { title: 'Traveling', desc: 'There is many more to visit.' },
+      { title: 'Private Driver', desc: 'Tired? Leave it to us.' },
+      { title: 'Package transport', desc: 'We take it fast and safely.' },
+      { title: 'And many more', desc: "Don't stop traveling!" },
+    ],
+    button: 'Book a trip!',
+  },
+  ES: {
+    h3: ['Somos ', 'Cherza'],
+    p: 'La agencia de transportes que estabas buscando. Te llevamos a ese viaje soñado, a una fiesta con tus amigos o a esa cita médica a la que no puedes faltar. ¡Puedes confiar en nosotros!',
+    services: [
+      { title: 'Aeropuerto', desc: 'Te acompañamos en tu próxima aventura.' },
+      { title: 'Hospitales', desc: 'No pierdas tu cita médica.' },
+      { title: 'Turismo', desc: 'Porque hay mucho por conocer.' },
+      {
+        title: 'Chofer particular',
+        desc: '¿Cansado? Déjalo en nuestras manos.',
+      },
+      { title: 'Paquetería', desc: 'Lo llevamos seguro y a tiempo.' },
+      { title: 'Y mucho más', desc: '¡Que tu vida no se detenga!' },
+    ],
+    button: '¡Quiero reservar!',
+  },
+}
+export const contacto = {
+  EN: {
+    contactSuccess: [
+      'We got your message, ',
+      'start packing',
+      " 'cause we are on our way",
+    ],
+    fields: ['Name', 'Last Name', 'Phone', 'Mail', 'Service', 'Passengers'],
+    button: 'Send',
+    h3: ['Begin a new ', 'adventure'],
+    p: 'Are you traveling alone or with friends/family? We got you covered! You are missing only one step to begin your adventure, contact us and enjoy our services.',
+  },
+  ES: {
+    contactSuccess: [
+      'Recibimos tu mensaje, ',
+      'comienza a empacar ',
+      ' pues ya vamos en camino',
+    ],
+    fields: [
+      'Nombre',
+      'Apellido',
+      'Teléfono',
+      'E-mail',
+      'Servicio',
+      'Pasajeros',
+    ],
+    button: 'Enviar',
+    h3: ['Comienza una nueva ', 'aventura'],
+    p: '¿Viajas solo o con tus amigos/familia? ¡Tenemos lo que necesitas! Estás a un paso de iniciar una nueva aventura, ponte en contacto con nosotros y comienza a disfutar de nuestro servicio.',
+  },
+}
+export const footer = {
+  EN: {},
+  ES: {},
+}
+
+export const cta = {
+  EN: 'book a trip',
+  ES: 'reservar',
+}
